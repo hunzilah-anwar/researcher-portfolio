d3859e0da6d5795f8bdb404f2255b89f6719eb4a
hunzilah-anwar <hunzilahanwar786@gmail.com>
2026-05-16 19:04:52 +0500

    responsiveness done

--- src/components/OtherCommon.jsx
+++ src/components/OtherCommon.jsx
@@ -2,14 +2,14 @@ const Heading = ({ name, darkColor }) => {
   return (
     <div className="relative flex items-center justify-center py-6">
       {/* Stroke Text */}
-      <h1
+      {/* <h1
         className="absolute text-6xl font-bold text-transparent opacity-20 cursor-default"
         style={{
           WebkitTextStroke: `1px ${darkColor || "#000"}`,
         }}
       >
         {name || "Heading"}
-      </h1>
+      </h1> */}
 
       {/* Main Text */}
       <h1 className="relative text-4xl font-bold text-center z-10 cursor-default">
