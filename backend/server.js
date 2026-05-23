e69bb13d1facf4443732523b3c646c9966c4609b
hunzilah_anwar <hunzilahanwar786@gmail.com>
2026-05-23 15:26:57 +0500

    All Done

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 // Enable CORS
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://researcher-portfolio-iota.vercel.app"
+  "https://drquratulainresearch.com"
 ];
 
 if (process.env.CORS_ORIGIN) {
